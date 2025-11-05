5783b46606408f57efd8fd364aad175c73f15494
arthurwarin <146280364+arthurwarin@users.noreply.github.com>
2025-11-05 11:25:00 +0100

    Update greeting message in app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return f"Hello from Whanos! Hostname: {os.environ.get('HOSTNAME', 'unknown')}"
+    return f"Hello from Whanos222222! Hostname: {os.environ.get('HOSTNAME', 'unknown')}"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
